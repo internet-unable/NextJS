98e967e1d35d614a28ea64a6cca15ae5f05c62b8
internet-unable <k_moiseenko@icloud.com>
2025-01-09 14:34:23 +0200

    Create NavLink
    - apply "use client";
    - add styles;
    - apply this component;

--- 02-foodies-app/components/MainHeader/MainHeader.js
+++ 02-foodies-app/components/MainHeader/MainHeader.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./MainHeader.module.css";
 import MainHeaderBackground from "./MainHeaderBackground/MainHeaderBackground";
+import NavLink from "./NavLink/NavLink";
 
 export default function MainHeader() {
     return (
@@ -25,10 +26,12 @@ export default function MainHeader() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href="/meals">Browse meals</Link>
+                            <NavLink href="/meals">Browse Meals</NavLink>
                         </li>
                         <li>
-                            <Link href="/community">Browse community</Link>
+                            <NavLink href="/community">
+                                Foodies Community
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
