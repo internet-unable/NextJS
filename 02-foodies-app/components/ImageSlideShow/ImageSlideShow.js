2d1eb8b232ff03a2cab5aaf39057d03555e703bb
internet-unable <k_moiseenko@icloud.com>
2025-01-09 14:33:30 +0200

    Convert styles for ImageSlideShow into module

--- 02-foodies-app/components/ImageSlideShow/ImageSlideShow.js
+++ 02-foodies-app/components/ImageSlideShow/ImageSlideShow.js
@@ -8,7 +8,7 @@ import macncheeseImg from "@/assets/macncheese.jpg";
 import pizzaImg from "@/assets/pizza.jpg";
 import schnitzelImg from "@/assets/schnitzel.jpg";
 import tomatoSaladImg from "@/assets/tomato-salad.jpg";
-import classes from "./image-slideshow.module.css";
+import classes from "./ImageSlideShow.module.css";
 
 const images = [
     { image: burgerImg, alt: "A delicious, juicy burger" },
