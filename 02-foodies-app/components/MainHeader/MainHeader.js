084bdec7dbc1c5857eeb49b43d6654786eed70aa
internet-unable <k_moiseenko@icloud.com>
2025-01-09 12:48:46 +0200

    Created MainHeader
    Created header for app. Add styles and learned how to use Image

---
+++ 02-foodies-app/components/MainHeader/MainHeader.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.png";
+import classes from "./MainHeader.module.css";
+
+export default function MainHeader() {
+    return (
+        <header className={classes.header}>
+            <Link href="/" className={classes.logo}>
+                <Image
+                    src={logoImg.src}
+                    width={80}
+                    height={80}
+                    alt="A plate with food on it"
+                    priority
+                />
+                NextLevel food
+            </Link>
+
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals">Browse meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Browse community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
