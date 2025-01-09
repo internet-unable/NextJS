8ec71255af23ee3b0c1beed5b2bcbff5c6bf5047
internet-unable <k_moiseenko@icloud.com>
2025-01-09 13:05:07 +0200

    Create MainHeaderBackground
    - move header background into component;
    - re-structure MainHeader folder;

--- 02-foodies-app/components/MainHeader/MainHeader.js
+++ 02-foodies-app/components/MainHeader/MainHeader.js
@@ -3,31 +3,36 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./MainHeader.module.css";
+import MainHeaderBackground from "./MainHeaderBackground/MainHeaderBackground";
 
 export default function MainHeader() {
     return (
-        <header className={classes.header}>
-            <Link href="/" className={classes.logo}>
-                <Image
-                    src={logoImg.src}
-                    width={80}
-                    height={80}
-                    alt="A plate with food on it"
-                    priority
-                />
-                NextLevel food
-            </Link>
+        <>
+            <MainHeaderBackground />
 
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Browse community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+            <header className={classes.header}>
+                <Link href="/" className={classes.logo}>
+                    <Image
+                        src={logoImg.src}
+                        width={80}
+                        height={80}
+                        alt="A plate with food on it"
+                        priority
+                    />
+                    NextLevel food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Browse community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 }
