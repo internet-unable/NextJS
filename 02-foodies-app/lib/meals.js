0efd1f9ec3a4d8a0783904ff01d005e0a5d28802
internet-unable <k_moiseenko@icloud.com>
2025-01-10 16:26:55 +0200

    Save shared meal to db
    - install some extra packages;
    - add saveMeal;

--- 02-foodies-app/lib/meals.js
+++ 02-foodies-app/lib/meals.js
@@ -1,4 +1,7 @@
+import fs from "node:fs";
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -11,4 +14,28 @@ export function getMeals() {
 
 export function getMealDetails(mealId) {
     return db.prepare("SELECT * FROM meals WHERE slug = ?").get(mealId);
+}
+
+export async function saveMeal(meal) {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+    const imageExtension = meal.image.name.split(".").pop();
+    const fileName = `${meal.slug}${Math.random()}.${imageExtension}`;
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error("Saving image failed!");
+        }
+    });
+
+    meal.image = `/images/${fileName}`;
+
+    db.prepare(
+        `
+        INSERT INTO meals (title, summary, instructions, image, creator, creator_email, slug)
+        VALUES (@title, @summary, @instructions, @image, @creator, @creator_email, @slug)
+        `
+    ).run(meal);
 }
