509afe48c86b9475e7d132018c459bd3c4ec994d
internet-unable <k_moiseenko@icloud.com>
2025-01-10 11:14:01 +0200

    Add measl section to the app

--- 02-foodies-app/app/meals/page.js
+++ 02-foodies-app/app/meals/page.js
@@ -1,5 +1,26 @@
-export default function MealsPage() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/Meals/MealsGrid/MealsGrid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
-        <h1>Meals page</h1>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose you favorite recipe and cook it yourself. It's easy and fun!</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share your favorite recipe</Link>
+                </p>
+            </header>
+
+            <main className={classes.main}>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
     );
 }
