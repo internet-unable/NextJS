19b9ed06d2e6a42c53f532b3f6095ae049b0f4a5
internet-unable <k_moiseenko@icloud.com>
2025-01-10 11:23:11 +0200

    Alternative approach of loading fallback

--- 02-foodies-app/app/meals/page.js
+++ 02-foodies-app/app/meals/page.js
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import Link from "next/link";
+
+import { getMeals } from "@/lib/meals";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/Meals/MealsGrid/MealsGrid";
-import { getMeals } from "@/lib/meals";
 
-export default async function MealsPage() {
+async function Meals() {
     const meals = await getMeals();
 
+    return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -19,7 +25,9 @@ export default async function MealsPage() {
             </header>
 
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
