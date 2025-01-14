e12895f3f8ec2378ff779ceb8176906dd6f2e7e0
internet-unable <k_moiseenko@icloud.com>
2025-01-14 14:07:54 +0200

    Update news/index
    - apply Link;

--- 03-nextjs-pages-router/pages/news/index.js
+++ 03-nextjs-pages-router/pages/news/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function NewsPage() {
     return(
-        <h1>The news page</h1>
+        <>
+            <h1>The news page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">NextJS is a great framework</Link>
+                </li>
+                <li>
+                    <Link href="news/something-else">Something else</Link>
+                </li>
+            </ul>
+        </>
     );
 }
