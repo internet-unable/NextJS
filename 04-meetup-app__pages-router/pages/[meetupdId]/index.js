6bdfa263a0b3832acbcd8722156a614abcfdaa85
internet-unable <k_moiseenko@icloud.com>
2025-01-14 18:02:58 +0200

    Update pages/[meetupId]/index
    - apply MeetupDetail;

--- 04-meetup-app__pages-router/pages/[meetupdId]/index.js
+++ 04-meetup-app__pages-router/pages/[meetupdId]/index.js
@@ -1,5 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
 export default function MeetupDetailPage() {
     return (
-        <h1>MeetupDetailPage page</h1>
+        <MeetupDetail 
+            image="https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg"
+            title="Meetup title"
+            address="Meetup address"
+            description="Meetup description"
+        />
     );
 }
