b0e2795f24c584281ba7a5a8a6401d580423c99d
internet-unable <k_moiseenko@icloud.com>
2025-01-14 15:01:45 +0200

    Add basic structure

---
+++ 04-meetup-app__pages-router/pages/[meetupdId]/index.js
@@ -0,0 +1,5 @@
+export default function MeetupDetailPage() {
+    return (
+        <h1>MeetupDetailPage page</h1>
+    );
+}
---
+++ 04-meetup-app__pages-router/pages/new-meetup/index.js
@@ -0,0 +1,5 @@
+export default function NewMeetupPage() {
+    return (
+        <h1>New meetup page</h1>
+    );
+}
