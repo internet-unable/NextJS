a14715ba907d581eb61aefd46228e31f94cd2479
internet-unable <k_moiseenko@icloud.com>
2025-01-14 18:59:21 +0200

    Update pages/index
    - apply getStaticProps;

--- 04-meetup-app__pages-router/pages/index.js
+++ 04-meetup-app__pages-router/pages/index.js
@@ -6,22 +6,30 @@ const DUMMY_MEETUPS = [
         title: "A first meetup",
         description: "This is a first meetup",
         address: "Some address 1, 12345 Some city",
-        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg"
+        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg",
     },
     {
         id: "m2",
         title: "A second meetup",
         description: "This is a second meetup",
         address: "Some address 2, 5678910 Some city",
-        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg"
-    }
-]
+        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg",
+    },
+];
 
-export default function HomePage() {
+export default function HomePage(props) {
     return (
         <>
             <h1>Home page</h1>
-            <MeetupList meetups={DUMMY_MEETUPS} />
+            <MeetupList meetups={props.meetups} />
         </>
     );
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS,
+        },
+    };
 }
