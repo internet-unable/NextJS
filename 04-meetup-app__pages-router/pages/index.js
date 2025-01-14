83d33b2a7e3a49205172661ffa751b93eabcf59b
internet-unable <k_moiseenko@icloud.com>
2025-01-14 15:44:02 +0200

    Update index
    - apply MeetupList;

--- 04-meetup-app__pages-router/pages/index.js
+++ 04-meetup-app__pages-router/pages/index.js
@@ -1,5 +1,27 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        title: "A first meetup",
+        description: "This is a first meetup",
+        address: "Some address 1, 12345 Some city",
+        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg"
+    },
+    {
+        id: "m2",
+        title: "A second meetup",
+        description: "This is a second meetup",
+        address: "Some address 2, 5678910 Some city",
+        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg"
+    }
+]
+
 export default function HomePage() {
     return (
-        <h1>Home page</h1>
+        <>
+            <h1>Home page</h1>
+            <MeetupList meetups={DUMMY_MEETUPS} />
+        </>
     );
 }
