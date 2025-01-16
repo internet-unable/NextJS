9368219aa7f7d78d12b39e50d298930eb161f649
internet-unable <k_moiseenko@icloud.com>
2025-01-16 16:17:09 +0200

    Update /pages/index.js
    Replace dummy data with request to db

--- 04-meetup-app__pages-router/pages/index.js
+++ 04-meetup-app__pages-router/pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "@/components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "A first meetup",
-        description: "This is a first meetup",
-        address: "Some address 1, 12345 Some city",
-        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg",
-    },
-    {
-        id: "m2",
-        title: "A second meetup",
-        description: "This is a second meetup",
-        address: "Some address 2, 5678910 Some city",
-        image: "https://vivo.tech/cdn/shop/articles/Blog_Cover_Image_2_800x.jpg",
-    },
-];
 
 export default function HomePage(props) {
     return (
@@ -27,9 +11,22 @@ export default function HomePage(props) {
 }
 
 export async function getStaticProps() {
+    const client = await MongoClient.connect("mongodb+srv://polocatic:1234@cluster0.zfuql.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0");
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+    const meetups = await meetupCollection.find().toArray();
+    
+    client.close();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS,
+            meetups: meetups.map(item => ({
+                id: item._id.toString(),
+                title: item.title,
+                address: item.address,
+                image: item.image
+            }))
         },
+        revalidate: 1
     };
 }
